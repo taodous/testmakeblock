65348579f3c7e3115ec85f74f07f1579709822b5
Tao Dous <tao.dous.junior@gmail.com>
2020-03-20 18:10:06 +0100

    Marche arriere v1

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,11 @@
 #include <MeMCore.h>
 #include <SoftwareSerial.h>
 #include <MeIR.h>
+#include <stdio.h>      
+#include <stdlib.h>  
+#include <time.h>
+
+
 
 MeDCMotor motor1(M1);
 MeDCMotor motor2(M2);
@@ -19,11 +24,16 @@ uint8_t motorSpeed = 0;
 
 //variable utilisee pour lire le code de la touche
 uint32_t value;
+ 
+//Sera utilisé pour faire un random 0 ou 1 pour choisir le sens de demi-tour
+int sens;
 
 
 
 void setup()
 {
+  
+  
   //Initialisation de la matrice LED
   ledMx.setBrightness(6);
   ledMx.setColorIndex(5);
@@ -44,49 +54,64 @@ void setup()
 void loop()
 {
 
+
+  // --------------------------------
+  // -- TELECOMMANDE ----------------
   //si une touche de la telecomamnde est pressée
   if (ir.decode()) {
     //recuperation de la touche
     value = ir.value;
     //Affichage de la valeur du code de la touche pressée
     Serial.println(value);
-  //Si la touche 0 est pressee, arret des moteurs 
-  if (value == 3910598400)
-    motorSpeed = 0;
+    //Si la touche 0 est pressee, arret des moteurs 
+    if (value == 3910598400)
+      motorSpeed = 0;
 
-  //Si la touche 1 est pressee, demarage des moteurs 
-  if (value == 4077715200)
-    motorSpeed = speed;
-  //si la touche fleche haut est pressee, augmenter la vitesse de 10 MAIS la bloquer a 250
-  if (value == 3208707840){
-    speed = speed + 10;
-    if (speed > 250)
-      speed = 250;
-    motorSpeed = speed;
+    //Si la touche 1 est pressee, demarage des moteurs 
+    if (value == 4077715200)
+      motorSpeed = speed;
+    //si la touche fleche haut est pressee, augmenter la vitesse de 10 MAIS la bloquer a 250
+    if (value == 3208707840){
+      speed = speed + 10;
+      if (speed > 250)
+        speed = 250;
+      motorSpeed = speed;
+    }
+    //si la touche fleche bas est pressee, reduire la vitesse de 10 MAIS la bloquer a 10
+    if (value == 3860463360){
+      speed = speed - 10;
+      if (speed < 0)
+        speed = 0;
+      motorSpeed = speed;
+    }
   }
-  //si la touche fleche bas est pressee, reduire la vitesse de 10 MAIS la bloquer a 10
-  if (value == 3860463360){
-    speed = speed - 10;
-    if (speed < 0)
-      speed = 0;
-    motorSpeed = speed;
-  }
-  
-    motor2.run(motorSpeed);
-  } 
+//--------------------- FIN TELECOMMANDE
+
 
   //Affichage sur la matrice LED de la distance en cm
   ledMx.showNum(ultraSensor.distanceCm());
  
   //Mise a jour de la vitesse des moteurs
+
+  //Mise a jour a chaque passage dans loop de la vitesse des moteurs
   motor1.run(-motorSpeed);
   motor2.run(motorSpeed);
 
   //Demi-tour pendant 1000ms si on detecte un obstacle a moins de 40
   if (ultraSensor.distanceCm() < 40 )
   {
-    motor1.run(motorSpeed);
-    motor2.run(motorSpeed);
+    //nombre aleatoire 0 ou 1
+    sens = rand()%2;
+    if (sens == 0){
+      motor1.run(motorSpeed);
+      motor2.run(motorSpeed);
+
+    }
+    else {
+      motor1.run(-motorSpeed);
+      motor2.run(-motorSpeed);
+    }
+
     //motor1.run(motorSpeed);
     //motor2.run(motorSpeed);
     //ledMx.drawStr(0,7,">>>");
@@ -94,13 +119,52 @@ void loop()
     //motor1.run(-motorSpeed);
     //motor2.run(motorSpeed);
 
-  while (ultraSensor.distanceCm()<40)//si la distance est inferieure a 40 cm alors tourner 
-  {
-       ledMx.drawStr(0,7,">>>");
-  }
 
-  }
-   }   
+    //On va demarrer une boucle tant que on est a moins de 40 cm
+    //On prends un chrono avant le demarrage pour pouvoir mesurer le temps
+    unsigned long int timeZero = millis();
+    //timer contient maintenant le temps actuel (TOP CHRONO)
+
+    while (ultraSensor.distanceCm()<40)
+    {
+
+      double duration = (millis() - timeZero) / 1000;
+      Serial.println("Duration: ");
+      Serial.println(duration);
+
+      if (sens == 0){
+        ledMx.drawStr(0,7,">>>"); 
+      }
+      else {
+        ledMx.drawStr(0,7,"<<<");
+      }
+
+      //Si on est face a un obstacle pendant moins de 5 secondes
+      if (duration < 5 )
+      {
+        if (sens == 0){
+          //Demi-tour sens 0
+          motor1.run(motorSpeed);
+          motor2.run(motorSpeed);
+        }
+        else {
+          //Demi-tour sens 1
+          motor1.run(-motorSpeed);
+          motor2.run(-motorSpeed);
+        }
+      }
+      else //Si on est face a un obstacle depuis plus ou egal de 5 secondes
+      {
+        //Marche arriere pendant deux secondes
+        motor1.run(motorSpeed);
+        motor2.run(-motorSpeed);
+        delay(2000);
+        motorSpeed = 0;
+        break ;
+      }
+    }//Fin while si on est a moins de 40 cm
+  }//Fin if on est moins de 40 cm
+}//Fin loop
 
   
   
